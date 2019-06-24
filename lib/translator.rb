7ca93bf419c2716c9c6132de3c69f3934374ff37
Tracie Masek <traciemasek@gmail.com>
2019-06-24 10:49:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,9 @@ def get_japanese_emoticon(file_path, emoticon)
     if emoticon == eng
       japan
     else
+      p "Sorry, that emoticon was not found"
+    end
+  end
 end
 
 def get_english_meaning(file_path, emoticon)
