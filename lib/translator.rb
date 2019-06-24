067d7bd07709bcefaddf9a2f8321633431f99382
Tracie Masek <traciemasek@gmail.com>
2019-06-24 10:44:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # code goes here
+  load_library(file_path)
 end
 
 def get_english_meaning(file_path, emoticon)
