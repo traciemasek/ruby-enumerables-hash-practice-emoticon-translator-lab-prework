ab604675dfab65f0c6eb3c42e3befd0387420ec8
Tracie Masek <traciemasek@gmail.com>
2019-06-24 11:04:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,13 +27,6 @@ def get_japanese_emoticon(file_path, emoticon)
     "Sorry, that emoticon was not found"
   end
   
-=begin  
-  if load_library(file_path)["get_emoticon"][emoticon]
-    load_library(file_path)["get_emoticon"][emoticon]
-  else
-    "Sorry, that emoticon was not found"
-  end
-=end 
 end
 
 def get_english_meaning(file_path, emoticon)
