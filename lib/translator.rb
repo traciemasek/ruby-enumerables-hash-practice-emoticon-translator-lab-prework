6ba297405437d194a3fc039e58560e9791ca0bef
Tracie Masek <traciemasek@gmail.com>
2019-06-24 11:00:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  #load_library(file_path)
   
   load_library(file_path)["get_emoticon"].each do |eng, japan|
     if eng == emoticon
